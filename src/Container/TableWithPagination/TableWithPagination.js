241749a892c730b90fa716fbc7b6982686bdcb0e
igobb <t.golab06@gmail.com>
2023-01-15 20:33:02 +0100

    Page loading setting, URL reflection, code refactoring

--- src/Container/TableWithPagination/TableWithPagination.js
+++ src/Container/TableWithPagination/TableWithPagination.js
@@ -30,6 +30,7 @@ export default function TableWithData() {
 
     //Getting data from API
     const [ourData, setOurData] = useState(null);
+    //Selecting row with data for modal
     const [selectedItem, setSelectedItem] = useState(null);
     const [open, setOpen] = useState(false);
     //Setting the page on which the table should be opened and the number of rows per page
@@ -39,7 +40,25 @@ export default function TableWithData() {
     const [query, setQuery] = useState('');
     //Storing error object
     const [errorData, setErrorData] = useState(null);
+    //Storing URL
+    const [queryParams, setQueryParams] = useState(null);
+    //When data is downloading from API
+    const [isLoading, setIsLoading] = useState(true);
 
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const searchInput = urlParams.get(`search`);
+        const page = urlParams.get(`page`);
+
+        searchInput && setQuery(searchInput);
+        page && setPage(+page);
+
+        setQueryParams({
+            searchParams: searchInput,
+            page: page
+        });
+    }, []);
 
     useEffect(() => {
         axios
@@ -47,13 +66,16 @@ export default function TableWithData() {
             .get('https://reqres.in/api/products')
             //Using 'setOurData' to storage data from API
             .then((response) => {
+                setIsLoading(false);
                 setOurData(response.data);
-                setPage(response.data.page - 1);
+                queryParams?.page && setPage(response.data.page - 1);
+
                 setRowsPerPage(response.data.per_page -1)
                 console.log('Data ok')
             })
             //Catching errors
             .catch((error) => {
+                setIsLoading(false);
                 //errors from 400 to 599
                 setErrorData(error)
                 if(error.request.status < 600 && error.request.status > 399) {
@@ -65,22 +87,27 @@ export default function TableWithData() {
             });
     }, []);
 
-
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const myParam = urlParams.get(`search`);
-
-        myParam && setQuery(myParam);
-    }, []);
-
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
+        handleChangeQueryParams(newPage, 'page');
     };
 
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
+
+    const handleSearchChange = (event) => {
+        setQuery(event.target.value);
+
+        handleChangeQueryParams(event.target.value,'search');
+    }
+
+    const handleChangeQueryParams = (value, paramName) => {
+        const url = new URL(window.location);
+        url.searchParams.set(paramName, value);
+        window.history.pushState(null, '', url.toString());
+    }
 
     const style = {
         position: 'absolute',
@@ -94,72 +121,83 @@ export default function TableWithData() {
         p: 4,
     };
 
+    const styleForInput = {
+        display: 'flex',
+        justifyContent: 'center',
+        margin: 'auto',
+        padding: '0.5rem 0.3rem',
+        paddingLeft: '1rem',
+        marginBottom: '1rem',
+        marginTop: '1rem',
+        fontSize: '1.5rem',
+        borderRadius: '10px'}
+
     return (
         <>
-            { ourData ?
-                <>
-                    <Paper sx={{ minWidth: '40%', overflow: 'hidden', backgroundColor: 'var(--secondary-color)' }} elevation={24} >
-                        <input
-                            //Blocking the possibility of entering text
-                            type="number"
-                            value={query}
-                            // value={query}
-                            placeholder="Filter data by ID"
-                            //Reaction to every change of the input field - storing the field value in 'setQuery'
-                            onChange={e => setQuery(e.target.value), e => {(e.target.value > 0 && e.target.value <= ourData.data.length) ? window.location.search = `search=${e.target.value}` : window.location.search = ''}}
-                            style={{display: 'flex', justifyContent: 'center', margin: 'auto', padding: '0.5rem 0.3rem', paddingLeft: '1rem', marginBottom: '1rem', marginTop: '1rem', fontSize: '1.5rem', borderRadius: '10px'}}/>
-                        <TableContainer component={Paper} sx={{width: 1}}>
-                            <Table aria-label="customized table">
-                                <TableHead>
-                                    <TableRow>
-                                        <StyledTableCell>ID</StyledTableCell>
-                                        <StyledTableCell align="right">Name</StyledTableCell>
-                                        <StyledTableCell align="right">Year</StyledTableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {ourData.data?.length > 0 &&
-                                        ourData.data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                            //Filtering the received array by product ID - will display the product with the same ID as stored in 'query'
-                                            .filter((someId) => someId.id.toString().includes(query))
-                                            .map(elements => {console.log(elements); return elements;})
-                                            .map((row) => (
-                                                <TableRow key={row.id} onClick={() => {setSelectedItem(row); setOpen(true)}}
-                                                          sx={{backgroundColor: row.color}}
-                                                >
-                                                    <StyledTableCell component="th" scope="row">
-                                                        {row.id}
-                                                    </StyledTableCell>
-                                                    <StyledTableCell align="right">
-                                                        {row.name}
-                                                    </StyledTableCell>
-                                                    <StyledTableCell align="right">
-                                                        {row.year}
-                                                    </StyledTableCell>
-                                                </TableRow>
-                                            ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        <TablePagination
-                            rowsPerPageOptions={[]}
-                            component="div"
-                            count= {ourData?.total}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                        />
-                    </Paper>
-                    {selectedItem && <Modal
-                        open={open}
-                        onClose={() => setOpen(false)}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
+            {isLoading ? <p>Loading...</p> : <>
+
+                { ourData ?
+                    <>
+                        <Paper sx={{ minWidth: '40%', overflow: 'hidden', backgroundColor: 'var(--secondary-color)' }} elevation={24} >
+                            <input
+                                //Blocking the possibility of entering text
+                                type="number"
+                                value={query}
+                                placeholder="Filter data by ID"
+                                //Reaction to every change of the input field - storing the field value in 'setQuery'
+                                onChange={handleSearchChange}
+                                style={styleForInput}/>
+                            <TableContainer component={Paper} sx={{width: 1}}>
+                                <Table aria-label="customized table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <StyledTableCell>ID</StyledTableCell>
+                                            <StyledTableCell align="right">Name</StyledTableCell>
+                                            <StyledTableCell align="right">Year</StyledTableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {ourData.data?.length > 0 &&
+                                            ourData.data?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                                //Filtering the received array by product ID - will display the product with the same ID as stored in 'query'
+                                                .filter((someId) => someId.id.toString().includes(query))
+                                                .map((row) => (
+                                                    <TableRow key={row.id} onClick={() => {setSelectedItem(row); setOpen(true)}}
+                                                              sx={{backgroundColor: row.color}}
+                                                    >
+                                                        <StyledTableCell component="th" scope="row">
+                                                            {row.id}
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="right">
+                                                            {row.name}
+                                                        </StyledTableCell>
+                                                        <StyledTableCell align="right">
+                                                            {row.year}
+                                                        </StyledTableCell>
+                                                    </TableRow>
+                                                ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <TablePagination
+                                rowsPerPageOptions={[]}
+                                component="div"
+                                count= {ourData?.total}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                        </Paper>
+                        {selectedItem && <Modal
+                            open={open}
+                            onClose={() => setOpen(false)}
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
                         >
                             <Box sx={style}>
                                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                                <p>{selectedItem.name.toUpperCase()} ALL DATA</p>
+                                    <p>{selectedItem.name.toUpperCase()} ALL DATA</p>
                                 </Typography>
                                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                                     <p>ID: {selectedItem.id}</p>
@@ -170,19 +208,20 @@ export default function TableWithData() {
                                 </Typography>
                             </Box>
                         </Modal>}
-                </>
-                :
-                <div className="error">
-                    {(errorData?.request.status < 600 && errorData?.request.status > 399) ?
-                        <>
-                            <h1>Sorry, we have a problem...</h1>
-                            <p>{errorData?.message}</p>
-                        </>:
-                        <>
-                            <h1>Sorry, we have a problem...</h1>
-                        </>}
-                </div>
-            }
+                    </>
+                    :
+                    <div className="error">
+                        {(errorData?.request.status < 600 && errorData?.request.status > 399) ?
+                            <>
+                                <h1>Sorry, we have a problem...</h1>
+                                <p>{errorData?.message}</p>
+                            </>:
+                            <>
+                                <h1>Sorry, we have a problem...</h1>
+                            </>}
+                    </div>
+                }
+            </>}
         </>
     );
 }
