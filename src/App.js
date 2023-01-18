555e6a0149e69b85d5bc1d31d842796fd1a2219a
igobb <t.golab06@gmail.com>
2023-01-18 18:48:37 +0100

    Refactoring
    Delete unused folders and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 
-// import Navbar from './Component/Navbar/Navbar'
 import TableWithPagination from './Container/TableWithPagination/TableWithPagination'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,7 +9,6 @@ import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div className="App">
-      {/*<Navbar/>*/}
         <TableWithPagination/>
     </div>
   );
--- src/Constans/images.js
+++
@@ -1,5 +0,0 @@
-import logo from '../Assets/logo.png'
-
-export default {
-    logo,
-};
